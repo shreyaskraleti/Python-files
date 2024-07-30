3bfded21e3f06a797f10d0b149e4a48deccb425f
Shreyas <shreyas.kraleti2@gmail.com>
2024-07-30 13:51:59 +0530

    type casting files are added

---
+++ Practise/typecasting.py
@@ -0,0 +1,17 @@
+# type casting: is a process of converting a variable from one data type to another.
+x = int(5.023)
+print(x)
+x = float(5)
+print(x)
+x = str(123)
+print(x)
+x = [1,2,3]
+print(tuple(x))
+x = list('123')
+print(x)
+x = [('a','1'),('b','2')]
+y = dict(x)
+print(y)
+x = (1,2,3)
+y = set(x)
+print(y)
