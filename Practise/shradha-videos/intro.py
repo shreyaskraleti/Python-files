b712912c4ca92fb76c8aa3467eb7b830ff952d42
Shreyas <shreyas.kraleti2@gmail.com>
2024-08-05 16:39:34 +0530

    variables and data types files are added

---
+++ Practise/shradha-videos/intro.py
@@ -0,0 +1,40 @@
+print("Hello World")
+print("my name is shreya")
+print("my age is 30")
+print("my name is shreya, my age is 30")
+print(23)
+print(30)
+print(23+30)
+print(23*2)
+
+name = "shreya" # string - within double quotes or single quotes
+age = 30
+price = 25.03
+print(name)
+print(age)
+print(price)
+
+print("my name is:", name, "and",  "my age is :", age)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+print("name")
+print('age')
+print('''address''')
+
+age = 25
+print(type(age))
+
+old = True
+print(type(old))
+
+a = 5.05
+print(type(a))
+
+x = "shreya"
+print(type(x))
+
+z = None
+print(type(z))
