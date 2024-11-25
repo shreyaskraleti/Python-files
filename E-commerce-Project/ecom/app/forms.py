d67e426f60d7861b6686ab7a8946d115ecb88841
Shreyas <shreyas.kraleti2@gmail.com>
2024-11-25 15:35:51 +0530

    ecom project files added

--- E-commerce-Project/ecom/app/forms.py
+++ E-commerce-Project/ecom/app/forms.py
@@ -1,23 +1,35 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField,PasswordChangeForm
 from django.contrib.auth.models import User
-
+from . models import Customer
+ 
 class LoginForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': 'True', 'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'form-control'}))
-
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus':'True', 'class':
+    'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+    'autocomplete':'current-password', 'class':'form-control'}))
+ 
 class CustomerRegistrationForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'autofocus': 'True', 'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    
+   
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
-        
+        fields = ['username',  'password1', 'password2','email',]
+ 
 class MyPasswordResetForm(PasswordChangeForm):
     pass
-
-class CustomerProfileForm(forms.ModelForm):
-    pass
+class CustomProfileForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['name', 'locality', 'city', 'mobile', 'state', 'zipcode']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}),
+            'locality': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your locality'}),
+            'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your city'}),
+            'mobile': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your mobile number'}),
+            'state': forms.Select(attrs={'class': 'form-control'}),  # Assuming state is a choice field
+            'zipcode': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your ZIP code'}),
+        }
--- E-commerce-Project/ecom/app/views.py
+++ E-commerce-Project/ecom/app/views.py
@@ -1,37 +1,38 @@
 from django.shortcuts import render
-from .models import Product
-from .forms import CustomerRegistrationForm  # Import the form here
+from .models import Product, Customer
 from django.views import View
+from . forms import CustomerRegistrationForm, CustomProfileForm
 from django.contrib import messages
-
+ 
+ 
+ 
+ 
 # Create your views here.
 def home(request):
-    return render(request, "app/home.html")
-
+    return render(request, 'app/home.html')
 def about(request):
-    return render(request, "app/about.html")
-
+    return render(request, 'app/about.html')
 def contact(request):
-    return render(request, "app/contact.html")
-
+    return render(request, 'app/contact.html')
+ 
 class CategoryView(View):
     def get(self, request, val):
         product = Product.objects.filter(category = val)
         title = Product.objects.filter(category=val).values('title')
-        return render(request, 'app/category.html', locals())
-    
+        return render(request, 'app/category.html',locals())
+   
 class CategoryTitle(View):
-    def get(self,request,val):
-        product = Product.objects.filter(title=val)
+    def get(self, request, val):
+        product = Product.objects.filter(title = val)
         title = Product.objects.filter(category=product[0].category).values('title')
-        return render(request,"app/category.html", locals())
-    
+        return render(request, 'app/category.html',locals())
+   
 class ProductDetail(View):
     def get(self,request,pk):  
         product = Product.objects.get(pk=pk)
         return render(request, 'app/productdetail.html', locals())
-
-class CustomerRegistrationView(View):
+   
+class CustomerRegistrtionView(View):
     def get(self,request):
         form = CustomerRegistrationForm()
         return render(request, 'app/customerregistration.html', locals())
@@ -43,10 +44,26 @@ class CustomerRegistrationView(View):
         else:
             messages.warning(request, "Invalid Input Data")
         return render(request, 'app/customerregistration.html', locals())
-    
 class ProfileView(View):
     def get(self,request):
+        form = CustomProfileForm()
         return render(request, 'app/profile.html', locals())
     def post(self,request):
+        form = CustomProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+            reg = Customer(user=user,name=name,city=city,locality=locality,mobile=mobile,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request, 'Congratulations!, Profile Saved Successfully')
+        else:
+            messages.warning(request,"Invalid Input Data")
         return render(request, 'app/profile.html', locals())
-    
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html', locals())
